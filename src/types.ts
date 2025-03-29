cd795eb5f9e47c40ff529a1844042e828cda2648
tooltipdev <192393975+tooltipdev@users.noreply.github.com>
2025-03-28 22:17:57 -0700

    feat: add additional types

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,4 @@
-import { ZodSchema, ZodOptional } from "zod";
+import { ZodSchema, ZodOptional, z } from "zod";
 
 /**
  * Provide additional functionality to Zod schemas.
@@ -33,7 +33,7 @@ export type PathOptions = {
    *
    * Providing `null` will omit the property from the output Zod schema.
    */
-  override?: ZodSchema | ZodOptional<ZodSchema>;
+  override?: UniversalSchema | null;
   // Additional metadata to be merged via zodSchemaWrapperMixin (ie ZodSchemaWrapper)
   metadata?: Record<string, any>;
   // Additional properties added to path targeted Object schemas.
@@ -43,34 +43,42 @@ export type PathOptions = {
 };
 
 export type LexiconToZodOptions = {
-  typeParserDict?: Partial<LexiconTypeParserMap>;
+  followRefs?: boolean;
+  typeParserDict?: Partial<TypeParserMap>;
   // Provide a Lexicon schema dictionary for `ref` lookups.
   lexiconDict?: Record<string, Record<string, any>>;
   // Dictionary of options applied to specific schema properties.
   pathOptions?: {
     // The full dot-notated path of the target schema property.
-    [schemaPropertyPath: string]: PathOptions;
+    [path: string]: PathOptions;
   };
 };
 
-export type LexiconTypeParser = (
+export type LexiconTypeParser<T> = (
   lexiconPartial: Record<string, any>,
   lexiconPropPath: string,
-  lexiconTypeParserMap: LexiconTypeParserMap,
+  lexiconTypeParserMap: TypeParserMap,
   options?: LexiconToZodOptions
-) => UniversalSchema;
+) => T;
 
-export type LexiconTypeParserMap = {
-  $default: LexiconTypeParser;
-  boolean: LexiconTypeParser;
-  integer: LexiconTypeParser;
-  string: LexiconTypeParser;
-  blob: LexiconTypeParser;
-  object: LexiconTypeParser;
-  array: LexiconTypeParser;
-  union: LexiconTypeParser;
-  ref: LexiconTypeParser;
-  [type: string]: LexiconTypeParser;
+export type PrimaryTypeParserMap = {
+  record: LexiconTypeParser<Record<string, any>>;
+  query: LexiconTypeParser<Record<string, any>>;
+  procedure: LexiconTypeParser<Record<string, any>>;
+  subscription: LexiconTypeParser<Record<string, any>>;
+};
+
+export type TypeParserMap = {
+  $default: LexiconTypeParser<UniversalSchema>;
+  boolean: LexiconTypeParser<UniversalSchema>;
+  integer: LexiconTypeParser<UniversalSchema>;
+  string: LexiconTypeParser<UniversalSchema>;
+  blob: LexiconTypeParser<UniversalSchema>;
+  object: LexiconTypeParser<UniversalSchema>;
+  array: LexiconTypeParser<UniversalSchema>;
+  union: LexiconTypeParser<UniversalSchema>;
+  ref: LexiconTypeParser<UniversalSchema>;
+  [type: string]: LexiconTypeParser<UniversalSchema>;
 };
 
 /**
@@ -85,8 +93,29 @@ export type UnionPath = `${string}.__union__.${number}`;
  * ATProtocol related types
  */
 
-export type LexiconPrimaryType =
-  | "query"
-  | "procedure"
-  | "subscription"
-  | "record";
+export const PrimaryTypeSchema = z.union([
+  z.literal("query"),
+  z.literal("procedure"),
+  z.literal("subscription"),
+  z.literal("record"),
+]);
+
+export const NonPrimaryTypeSchema = z.union([
+  z.literal("null"),
+  z.literal("boolean"),
+  z.literal("integer"),
+  z.literal("string"),
+  z.literal("bytes"),
+  z.literal("cid-link"),
+  z.literal("blob"),
+  z.literal("array"),
+  z.literal("object"),
+  z.literal("params"),
+  z.literal("token"),
+  z.literal("ref"),
+  z.literal("union"),
+  z.literal("unknown"),
+]);
+
+export type LexiconPrimaryType = z.infer<typeof PrimaryTypeSchema>;
+export type NonPrimaryType = z.infer<typeof NonPrimaryTypeSchema>;
