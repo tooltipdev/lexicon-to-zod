ff2697c36235334130d1e008281be78e270fe790
tooltipdev <192393975+tooltipdev@users.noreply.github.com>
2025-03-31 13:15:30 -0700

    feat: export all parsers and modify parser map methods interface

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,48 @@
-import { z } from "zod";
 import {
-  toArraySchema,
-  toBlobRefSchema,
-  toBooleanSchema,
-  toBytesSchema,
-  toCidLinkSchema,
-  toNullSchema,
-  toNumberSchema,
-  toObjectSchema,
-  toProcedureSchemaMap,
-  toQuerySchemaMap,
-  toRecordSchemaMap,
-  toRefSchema,
-  toStringSchema,
-  toSubscriptionSchemaMap,
-  toUnionSchema,
-  toUnknownSchema,
+  toArraySchema as toArraySchemaParser,
+  toBlobRefSchema as toBlobRefSchemaParser,
+  toBooleanSchema as toBooleanSchemaParser,
+  toBytesSchema as toBytesSchemaParser,
+  toCidLinkSchema as toCidLinkSchemaParser,
+  toNullSchema as toNullSchemaParser,
+  toNumberSchema as toNumberSchemaParser,
+  toObjectSchema as toObjectSchemaParser,
+  toProcedureSchemaMap as toProcedureSchemaMapParser,
+  toQuerySchemaMap as toQuerySchemaMapParser,
+  toRecordSchemaMap as toRecordSchemaMapParser,
+  toRefSchema as toRefSchemaParser,
+  toStringSchema as toStringSchemaParser,
+  toSubscriptionSchemaMap as toSubscriptionSchemaMapParser,
+  toUnionSchema as toUnionSchemaParser,
+  toUnknownSchema as toUnknownSchemaParser,
 } from "./parsers";
+
 import { LexiconToZodOptions, TypeParserMap } from "./types";
 import { getTypeParserSafe, setPathOptionToIsRequired } from "./utils";
+import { z } from "zod";
 
-// Merge with `LexiconToZodOptions.typeParserDict`.
+/**
+ * Export individual type parsers for convenience.
+ */
+
+export const toArraySchema = toArraySchemaParser;
+export const toBlobRefSchema = toBlobRefSchemaParser;
+export const toBooleanSchema = toBooleanSchemaParser;
+export const toBytesSchema = toBytesSchemaParser;
+export const toCidLinkSchema = toCidLinkSchemaParser;
+export const toNullSchema = toNullSchemaParser;
+export const toNumberSchema = toNumberSchemaParser;
+export const toObjectSchema = toObjectSchemaParser;
+export const toProcedureSchemaMap = toProcedureSchemaMapParser;
+export const toQuerySchemaMap = toQuerySchemaMapParser;
+export const toRecordSchemaMap = toRecordSchemaMapParser;
+export const toRefSchema = toRefSchemaParser;
+export const toStringSchema = toStringSchemaParser;
+export const toSubscriptionSchemaMap = toSubscriptionSchemaMapParser;
+export const toUnionSchema = toUnionSchemaParser;
+export const toUnknownSchema = toUnknownSchemaParser;
+
+// Default TypeParserMap for internal use; merged with typeParserDict input.
 const defaultTypeParserMap: TypeParserMap = {
   $default: () => z.any(),
   string: toStringSchema,
@@ -37,6 +59,7 @@ const defaultTypeParserMap: TypeParserMap = {
   "cid-link": toCidLinkSchema,
 };
 
+// Map of primary type parsers for internal use.
 const primaryTypeParserMap = {
   record: toRecordSchemaMap,
   subscription: toSubscriptionSchemaMap,
@@ -45,8 +68,8 @@ const primaryTypeParserMap = {
 };
 
 /**
- * Infer a map of Zod schemas from a Lexicon `refs` dictionary.
- * @param lexicon Lexicon JSON definition
+ * Convert a Lexicon document into a Zod schema map.
+ * @param lexicon Lexicon document
  * @returns UniversalSchema map
  */
 export function lexiconToZod(
@@ -90,11 +113,12 @@ export function lexiconToZod(
 }
 
 /**
- * Convenience factory method; returns a map of all built-in primary and non-primary type parsers.
- * @returns Map of built-in parsers
+ * Return map of all built-in type parsers.
+ * Each type parser is modified to match the interface of `lexiconToZod`.
+ * @returns built-in parser map
  */
 export function parsers() {
-  return {
+  return Object.entries({
     record: toRecordSchemaMap,
     query: toQuerySchemaMap,
     procedure: toProcedureSchemaMap,
@@ -111,5 +135,27 @@ export function parsers() {
     bytes: toBytesSchema,
     unknown: toUnknownSchema,
     "cid-link": toCidLinkSchema,
-  };
+  }).reduce(
+    (acc, [type, typeParser]) => {
+      acc[type] = (
+        lexiconPartial: Record<string, any>,
+        options: LexiconToZodOptions = {}
+      ) => {
+        options.typeParserDict = Object.assign(
+          {},
+          defaultTypeParserMap,
+          options.typeParserDict || {}
+        );
+
+        options.pathOptions = options.pathOptions || {};
+        options.pathOptions[""] = options.pathOptions[""] || {};
+        options.pathOptions[""].isRequired = true;
+
+        return typeParser(lexiconPartial, "", options);
+      };
+
+      return acc;
+    },
+    {} as Record<string, Function>
+  );
 }
